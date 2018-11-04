77124aac149e0f61e80510b31d363ebcfb0760a6
tmig <tmig@yourlabs.org>
2018-11-04 01:24:12 +0100

    readmode, private communication, less greedy in cpu and time

--- processcontroller/__init__.py
+++ processcontroller/__init__.py
@@ -2,6 +2,7 @@ import os
 import pty
 import re
 import signal
+import time
 
 from queue import Queue, Empty
 from threading import Thread
@@ -13,25 +14,38 @@ OUT = 0
 
 class _Reader:
 
-    def __init__(self, stream):
+    def __init__(self, stream, mode='line'):
         self._s = stream
         self._q = Queue()
-        self.read = True
+        self.reading = True
+        self.readmode = mode
+        self.line = ''
 
         def _populateQueue(stream, queue):
-            while self.read:
-                char = os.read(stream, 1).decode()
-                queue.put(char)
+            if self.readmode == 'line':
+                while self.reading:
+                    self.line = ''
+                    while not len(self.line) or self.line[-1] != '\n':
+                        self.line += os.read(stream, 1).decode()
+                    queue.put((self.line, '\n'))
+            else:
+                char = ''
+                while self.reading:
+                    if char == '\n':
+                        self.line = ''
+                    char = os.read(stream, 1).decode()
+                    self.line += char
+                    queue.put((self.line, char))
 
         self._t = Thread(target=_populateQueue, args=(self._s, self._q))
         self._t.daemon = True
         self._t.start()
 
-    def readchar(self, timeout=None):
+    def read(self, timeout=None):
         try:
             return self._q.get(block=timeout is not None, timeout=timeout)
         except Empty:
-            return None
+            return None, None
 
 
 class ProcessController():
@@ -44,9 +58,15 @@ class ProcessController():
         self.thread = self
         self.pid = 0
         self.closed = False
+        self.private = False
 
     def __handle_line(self, line, char):
-        os.write(pty.STDOUT_FILENO, char.encode())
+        if not self.private:
+            if self.readmode == 'line':
+                os.write(pty.STDOUT_FILENO, line.encode())
+            else:
+                os.write(pty.STDOUT_FILENO, char.encode())
+
         if 'when' in self.options:
             for w in self.options['when']:
                 pattern = re.compile(w[0])
@@ -64,31 +84,26 @@ class ProcessController():
 
             self.send(instr)
 
+    def __empty_buffer(self):
+        line, char = self.reader.read(0.01)
+        while line or char:
+            self.__handle_line(line, char)
+            line, char = self.reader.read(0.01)
+
     def __loop(self):
         try:
             ret = os.waitpid(self.pid, os.WNOHANG)
-            line = ''
             while ret == (0, 0):
-                char = self.reader.readchar(0.01)
-                if char:
-                    line += char
+                line, char = self.reader.read(0.01)
+                if line or char:
                     self.__handle_line(line, char)
-                if char == '\n':
-                    line = ''
                 ret = os.waitpid(self.pid, os.WNOHANG)
 
         except ChildProcessError:
             pass
 
         finally:
-            char = self.reader.readchar(0.01)
-            while char:
-                line += char
-                self.__handle_line(line, char)
-                if char == '\n':
-                    line == ''
-                char = self.reader.readchar(0.01)
-            self.reader.read = False
+            self.__empty_buffer()
             self.return_value = ret
 
     def __fork(self, cmd):
@@ -97,7 +112,8 @@ class ProcessController():
             self.pid = pid
             os.close(self.pipes['parent_to_child'][OUT])
             os.close(self.pipes['child_to_parent'][IN])
-            self.reader = _Reader(self.pipes['child_to_parent'][OUT])
+            self.reader = _Reader(self.pipes['child_to_parent'][OUT],
+                                  self.readmode)
             self.__input()
             self.__loop()
 
@@ -127,12 +143,27 @@ class ProcessController():
 
         self.options = opt
 
+        if 'private' in opt and opt['private']:
+            self.private = True
+
+        if 'readmode' in opt and opt['readmode'] == 'char':
+            self.readmode = 'char'
+        else:
+            self.readmode = 'line'
+
         if 'detached' in opt and opt['detached'] is True:
             self.thread = Thread(target=self.__fork, args=(cmd,))
             self.thread.daemon = True
             self.thread.start()
         else:
             self.__fork(cmd)
+        return self.return_value
+
+    def wait(self):
+        while self.return_value == (0, 0):
+            time.sleep(0.1)
+            continue
+        self.__empty_buffer()
         return self.return_value
 
     def close(self):
