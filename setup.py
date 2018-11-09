35def927dadefb495c9929de3a89b927633b5afd
jpic <jpic@yourlabs.org>
2018-11-09 13:41:37 +0100

    Setupmeta

--- setup.py
+++ setup.py
@@ -1,29 +1,19 @@
 from setuptools import setup
-import os
-
-
-# Utility function to read the README file.
-# Used for the long_description. It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-VERSION = '0.0.3'
 
 
 setup(
     name='processcontroller',
-    version=VERSION if '@' not in VERSION else 'dev',
-    description='Replacement for pexpect',
+    versioning='distance',
+    setup_requires='setupmeta',
+    url='https://yourlabs.io/oss/processcontroller',
+    description='Alternative pexpect for linux and python3',
     author='Thomas Mignot',
     author_email='tmig@yourlabs.org',
-    url='https://github.com/thommignot/processcontroller',
-    packages=['processcontroller'],
-    include_package_data=True,
-    long_description=read('README.rst'),
     license='MIT',
     keywords='pexpect subprocess',
     python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+    ],
 )
